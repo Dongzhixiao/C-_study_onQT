ad0a9e5d5660c8228814eb338cac2167e56c4cda
Xiaodong_Wang <dongzhixiaodong@gmail.com>
2016-05-02 15:47:36 +0800

    add self-defined button example

--- imageButton.h
+++ imageButton.h
@@ -5,7 +5,7 @@
 /*继承QAbstractButton的原因：
  * 1.已经处理了各个事件，能正常响应触摸操作
  * 2.有pressed()和released()两个信号可以反馈下压和释放两种状态
- * 3.重载painEvent()函数可以改变按钮形状(QWidget类共有的特点)
+ * 3.重载painEvent()函数可以改变按钮形状(QWidget子类共有的特点)
  */
 class ImageButton : public QAbstractButton
 {
@@ -21,8 +21,8 @@ protected:
     void paintEvent(QPaintEvent *e);
 
 protected:
-    QPixmap m_normal;
-    QPixmap m_pressed;
+    QPixmap m_normal;   //正常状态图片
+    QPixmap m_pressed;  //下压状态图片
     bool m_bPressed;
 };
 
